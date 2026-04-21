1a0923a000e17e19a698b0d337c60dbf565256a2
oakinh <nikaohill@gmail.com>
2026-04-21 07:04:47 -0500

    Add push function with tested bubble up

--- include/systems_dsa/binary_heap.hpp
+++ include/systems_dsa/binary_heap.hpp
@@ -2,6 +2,12 @@
 #include <functional>
 #include <systems_dsa/vector.hpp>
 
+#ifndef NDEBUG
+#define BHEAP_ASSERT_VALID() assertValid();
+#else
+#define BHEAP_ASSERT_VALID() (void(0))
+#endif
+
 namespace systems_dsa {
 
 template <typename T, typename Compare = std::less<T>>
@@ -42,16 +48,60 @@ public:
     // Element access (top)
     // =========================
 
+    reference top() const {
+        return m_data[0];
+    }
+
     // =========================
     // Modifiers (push, emplace, pop)
     // =========================
+    void push(value_type val) {
+        m_data.push_back(val);
+        size_type insertedIndex { m_data.size() - 1 };
+
+        if (insertedIndex == 0) return;
+
+        for (
+            size_type parentIndex { getParentIndex(insertedIndex) };
+            m_comp(m_data[insertedIndex], m_data[parentIndex]);
+            insertedIndex = parentIndex, parentIndex = getParentIndex(insertedIndex)
+        ) {
+            std::swap(m_data[insertedIndex], m_data[parentIndex]);
+            if (parentIndex == 0) break;
+        }
+        BHEAP_ASSERT_VALID();
+    }
 
 private:
     // =========================
     // Data members
     // =========================
     systems_dsa::vector<value_type> m_data {};
-    Compare m_comparator;
+    comparator_type m_comp;
+
+    size_type getParentIndex(size_type i) const {
+        assert(i > 0);
+        return (i - 1) / 2;
+    }
+
+    size_type getLeftChildIndex(size_type i) const {
+        assert(i > 0);
+        return 2 * i + 1;
+    }
+
+    size_type getRightChildIndex(size_type i) const {
+        assert(i > 0);
+        return 2 * i + 2;
+    }
+
+    void assertValid() const {
+        for (size_type i { 1 }; i < m_data.size(); ++i) {
+            // Initialize to 1 to avoid dividing by zero in get...Index helpers
+            size_type parentIndex { getParentIndex(i) };
+            assert(m_comp(m_data[parentIndex], m_data[i]) && "assertValid() detected an order rule violation");
+            assert(m_comp(top(), m_data[i]) && "top() providing m_data[0] was not the highest priority element");
+        }
+    }
 };
 
 }
--- tests/binary_heap_test.cpp
+++ tests/binary_heap_test.cpp
@@ -5,3 +5,13 @@ TEST(BinaryHeapTest, DefaultInitializationYieldsSizeZero) {
     systems_dsa::binary_heap<int> heap {};
     EXPECT_EQ(heap.size(), 0);
 }
+
+TEST(BinaryHeapTest, PushPreservesOrderProperty) {
+    systems_dsa::binary_heap<int, std::less<>> heap {};
+    heap.push(10);
+    heap.push(2);
+    heap.push(55);
+    heap.push(33);
+    heap.push(12);
+    heap.push(1000);
+}
